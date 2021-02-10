d36d140f8979c68d259f1e51dd4c73b9501fac56
Piotr Krasny <krasny.piotr@gmail.com>
2021-02-10 13:04:59 +0100

    Clean

--- src/components/ProjectList/ProjectsList.js
+++ src/components/ProjectList/ProjectsList.js
@@ -1,10 +1,7 @@
-import React, { useContext } from "react";
+import React from "react";
 import Project from "../Project/Project";
-import AppContext from "../../context";
 
 const ProjectList = ({ list, status }) => {
-  const { allProjects } = useContext(AppContext);
-
   return (
     <div>
       {/* <h2>{list.length === 0 ? "Add first task" : "Active tasks:"}</h2> */}
