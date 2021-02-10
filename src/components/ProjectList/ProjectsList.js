4edb6a63ecedae80c090374136e56117b5f2aad4
Piotr Krasny <krasny.piotr@gmail.com>
2021-02-10 13:34:10 +0100

    Style

--- src/components/ProjectList/ProjectsList.js
+++ src/components/ProjectList/ProjectsList.js
@@ -4,8 +4,15 @@ import Project from "../Project/Project";
 const ProjectList = ({ list, status }) => {
   return (
     <div>
-      {/* <h2>{list.length === 0 ? "Add first task" : "Active tasks:"}</h2> */}
-      <h2>{status === "active" ? "Active tasks" : "Completed tasks:"}</h2>
+      <h2>
+        {status === "active"
+          ? list.length === 0
+            ? "Add first task"
+            : "Active tasks"
+          : list.length === 0
+          ? null
+          : "Completed tasks:"}
+      </h2>
       <ul style={{ padding: 0 }}>
         {list.map((item) => (
           <Project key={item.id} id={item.id} status={status} />
